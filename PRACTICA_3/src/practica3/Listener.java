874ede0c4a73b6d09687d18fa529c7882d907ba4
romanlarrosa <58295696+romanlarrosa@users.noreply.github.com>
2020-12-11 17:59:49 +0100

    Creada practica 3

---
+++ PRACTICA_3/src/practica3/Listener.java
@@ -0,0 +1,105 @@
+package practica3;
+
+import IntegratedAgent.IntegratedAgent;
+import Map2D.Map2DGrayscale;
+import YellowPages.YellowPages;
+import jade.core.AID;
+import jade.lang.acl.ACLMessage;
+import static ACLMessageTools.ACLMessageTools.getDetailsLARVA;
+import java.util.ArrayList;
+
+
+/**
+ * Agente que se dedica a escuchar lo que pasa en el mundo. 
+ * @author 
+ */
+public class Listener extends IntegratedAgent {
+
+    protected YellowPages myYP;
+    protected String myStatus, myService, myWorldManager, myWorld, myConvID;
+    protected boolean myError;
+    protected ACLMessage in, out;
+    protected Map2DGrayscale myMap;
+
+    @Override
+    public void setup() {
+        _identitymanager = "Sphinx";
+        super.setup();
+
+        Info("Booting");
+
+        // Description of my group
+        myService = "Analytics group Cellnex";
+
+        // The world I am going to open
+        myWorld = "World1";
+
+        // First state of the agent
+        myStatus = "CHECKIN-LARVA";
+
+        // To detect possible errors
+        myError = false;
+
+        _exitRequested = false;
+    }
+
+    @Override
+    public void plainExecute() {
+        switch (myStatus.toUpperCase()) {
+            case "CHECKIN-LARVA":
+                Info("Haciendo el checkin en LARVA con " + _identitymanager);
+                in = sendCheckinLARVA(_identitymanager);
+                myError = (in.getPerformative() != ACLMessage.INFORM);
+                if (myError) {
+                    Info("\t" + ACLMessage.getPerformative(in.getPerformative())
+                            + " El checkin ha fallado por: " + getDetailsLARVA(in));
+                    myStatus = "EXIT";
+                    break;
+                }
+                myStatus = "CHECKOUT-LARVA";
+                break;
+            case "CHECKOUTT-LARVA":
+                Info("Haciendo checkout de LARVA en" + _identitymanager);
+                in = sendCheckoutLARVA(_identitymanager);
+                myStatus = "EXIT";
+                break;
+            
+            case "EXIT":
+                Info("El agente muere");
+                _exitRequested = true;
+                break;
+            
+
+        }
+    }
+
+    @Override
+    public void takeDown() {
+        Info("Taking down");
+        super.takeDown();
+    }
+
+    private ACLMessage sendCheckinLARVA(String im) {
+        out = new ACLMessage();
+        out.setSender(getAID());
+        out.addReceiver(new AID(im, AID.ISLOCALNAME));
+        out.setContent("");
+        out.setProtocol("ANALYTICS");
+        out.setEncoding(_myCardID.getCardID());
+        out.setPerformative(ACLMessage.SUBSCRIBE);
+        send(out);
+        return blockingReceive();
+    }
+    
+    private ACLMessage sendCheckoutLARVA(String im) {
+        out = new ACLMessage();
+        out.setSender(getAID());
+        out.addReceiver(new AID(im, AID.ISLOCALNAME));
+        out.setContent("");
+        out.setProtocol("ANALYTICS");
+        out.setEncoding(_myCardID.getCardID());
+        out.setPerformative(ACLMessage.CANCEL);
+        send(out);
+        return blockingReceive();
+    }
+}
---
+++ PRACTICA_3/src/practica3/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica3;
+
+import AppBoot.ConsoleBoot;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ConsoleBoot app = new ConsoleBoot("HACKATHON", args);
+        app.selectConnection();
+        
+        //app.launchAgent("76628586BBBB", Listener.class);
+        app.launchAgent("Pantoja", Pantoja.class);
+        
+        app.shutDown();        
+    }
+    
+}
